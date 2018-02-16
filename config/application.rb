63d6bdf32bd0630eacd2161a1bbe7e065772f7fc
tochman <thomas@craftacademy.se>
2018-02-16 07:45:03 +0100

    sets up testing frameworks and generator defaults

--- config/application.rb
+++ config/application.rb
@@ -18,14 +18,14 @@ Bundler.require(*Rails.groups)
 
 module PayWallDemo
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-    # Don't generate system test files.
-    config.generators.system_tests = nil
+    config.generators do |generate|
+      generate.helper false
+      generate.assets false
+      generate.view_specs false
+      generate.helper_specs false
+      generate.routing_specs false
+      generate.controller_specs false
+    end
   end
 end
