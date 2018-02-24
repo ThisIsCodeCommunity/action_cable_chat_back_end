6ed3b85760a2e6687d98e80ae753e662e1a93dbd
tochman <thomas@craftacademy.se>
2018-02-24 09:28:41 +0100

    sets wss server in production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.url = 'wss://cnc-chat.herokuapp.com/cable'
+
+  config.action_cable.allowed_request_origins = [/http:\/\/*/,
+                                                 /https:\/\/*/]
 end
