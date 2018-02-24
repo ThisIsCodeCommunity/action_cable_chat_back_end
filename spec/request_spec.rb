9f1a97785169fbd3fb67a40c6fbb6ccbd281b1a9
magnus-thor <magnus.thor.ar@gmail.com>
2018-02-24 13:36:18 +0100

    setup request spec for fetching channels

---
+++ spec/request_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe ChatRoomsController, type: :request do
+  describe 'GET /' do
+    context 'return 1 chat room' do
+      let!(:chat_room) {FactoryBot.build(:chat_room, host: 'Oliver', title: 'General Chat')}
+
+      it 'should return a chat room' do
+        get '/'
+
+        expect(response.status).to eq 200
+        expected_response = {id: 1, host: 'Oliver', title: 'General Chat'}
+        expect(response_json).to eq expected_response.as_json
+      end
+    end
+  end
+end
